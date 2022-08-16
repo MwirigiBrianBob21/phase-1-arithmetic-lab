62e00543994fbf10cb3d4a7adb4a741a94376b53
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-08-17 02:21:23 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1=31;
+const num2=2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random()*10) + 1;
+const max = Math.max(2, 20, 15, 1);
+const num3 = 20;
+const num4 = 8;
+const mod = num3%num4;
